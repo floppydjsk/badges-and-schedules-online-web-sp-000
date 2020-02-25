d5f4cd2ff8f54a36bba89cbe439d6a14f31d9457
Daniel Kim <daniel.kim52@gmail.com>
2020-02-25 10:53:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,4 @@
-name = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 def badge_maker(name)
-  puts "Hello, my name is #{name.each}"
+  puts "Hello, my name is #{name}."
 end
-  
